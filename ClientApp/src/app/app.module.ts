b85f58489ffb47880a0611dd001ebbe2271130c2
sendeveloper <lightmorning46@gmail.com>
2018-09-11 19:34:42 +0300

    added the pages

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -5,11 +5,17 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '@app/shared';
 import { CoreModule } from '@app/core';
 
+import { SchoolModule } from './school/school.module';
+import { SemesterModule } from './semester/semester.module';
+import { PeriodModule } from './period/period.module';
+import { RoomModule } from './room/room.module';
+
+
+
 import { StudentModule } from './student/student.module';
 import { TeacherModule } from './teacher/teacher.module';
 import { ClassModule } from './class/class.module';
 import { LessonModule } from './lesson/lesson.module';
-import { SemesterModule } from './semester/semester.module';
 import { ScheduleModule } from './schedule/schedule.module';
 import { SettingsModule } from './settings';
 import { StaticModule } from './static';
